95f04aa654b781fdf26835a09bd724175748f2d8
Moji <mojtabavahabnezhad@gmail.com>
2024-08-27 23:38:27 +0330

    make new changes :/

---
+++ src/contact.js
@@ -0,0 +1,16 @@
+function contact(template){
+    template.innerHTML = ` <h1 class="about">About</h1>
+            <p class="about-para">Beary's has the best porridge! The atmosphere and customer service make you feel like you are sitting in the middle of the woods, eating like a bear! This is exactly the kind of place that I like to return to again and again.</p>
+            <h1 class="hours">Hours</h1>
+            <p class="hours-para">Sunday: 8am - 8pm
+                <br>Monday: 6am - 6pm
+                <br>Tuesday: 6am - 6pm
+                <br>Wednesday: 6am - 6pm
+                <br>Thursday: 6am - 10pm
+                <br>Friday: 6am - 10pm
+                <br>Saturday: 8am - 10pm</p>
+            <h1 class="location">Location</h1>
+            <p class="location-para">123 Forest Drive, Forestville, Maine</p>`
+}
+
+export {contact};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./style.css";
+import { home } from "./home";
+import { menu } from "./menu";
+import { contact } from "./contact";
 
-console.log("hihi")
+const template = document.querySelector(".template");
+
+document.addEventListener("click", (e)=>{
+    if (e.target.id == "home"){
+        home(template);
+    } else if (e.target.id == "menu"){
+        menu(template);
+    } else if (e.target.id == "contact"){
+        contact(template)
+    }
+})
